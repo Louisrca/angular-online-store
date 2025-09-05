bc07febab038fdc21dfe6059a17d9c9bdd093877
LouisR17 <roccalouis26@gmail.com>
2025-09-05 17:04:56 +0200

    feat: style product list

--- src/app/infrastructures/mocks/produts.ts
+++ src/app/infrastructures/mocks/produts.ts
@@ -5,21 +5,64 @@ export const Products: Product[] = [
     id: 1,
     name: 'product.one',
     description: 'Description for Product 1',
-    price: 100,
-    imageUrl: 'https://via.placeholder.com/150',
+    price: '100 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/61/0/8218610.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
   },
   {
     id: 2,
     name: 'product.two',
     description: 'Description for Product 2',
-    price: 200,
-    imageUrl: 'https://via.placeholder.com/150',
+    price: '200 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/12/5/8386125.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
   },
   {
     id: 3,
     name: 'product.three',
     description: 'Description for Product 3',
-    price: 300,
-    imageUrl: 'https://via.placeholder.com/150',
+    price: '300 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/18/3/8288183.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
+  },
+  {
+    id: 4,
+    name: 'product.four',
+    description: 'Description for Product 4',
+    price: '400 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/34/3/6364343.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
+  },
+  {
+    id: 5,
+    name: 'product.five',
+    description: 'Description for Product 5',
+    price: '500 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/34/3/6364343.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
+  },
+  {
+    id: 6,
+    name: 'product.six',
+    description: 'Description for Product 6',
+    price: '600 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/34/3/6364343.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
+  },
+  {
+    id: 7,
+    name: 'product.seven',
+    description: 'Description for Product 7',
+    price: '700 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/34/3/6364343.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
+  },
+  {
+    id: 8,
+    name: 'product.eight',
+    description: 'Description for Product 8',
+    price: '800 €',
+    imageUrl:
+      'https://media-cdn.citadium.com/image/34/3/6364343.png?fit=bounds&bg-color=EBEDEF&width=500&height=750&canvas=500-750',
   },
 ];
