080645a8071c60a72eab0d33c33746f9e38de686
LouisR17 <roccalouis26@gmail.com>
2025-09-10 14:53:34 +0200

    feat: update product mock

--- src/app/features/shop/models/products.model.ts
+++ src/app/features/shop/models/products.model.ts
@@ -1,7 +1,12 @@
 export interface Product {
   id: number;
   name: string;
+  type?: string;
+  gender?: string;
+  availabledSize?: string[];
+  color?: string;
   description: string;
   price: string;
   imageUrl: string;
+  imageUrls?: string[];
 }
