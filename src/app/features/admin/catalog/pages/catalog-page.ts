14264160d8e3a3b24f8f4fc6be6acd9a13ce5019
LouisR17 <roccalouis26@gmail.com>
2025-09-22 22:33:19 +0200

    feat: update title on catalog page

--- src/app/features/admin/catalog/pages/catalog-page.ts
+++ src/app/features/admin/catalog/pages/catalog-page.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BaseComponent } from '@Shared/components/base-translate/base-translate';
 import { TRANSLATE_IMPORTS } from '@Shared/imports/translate-imports';
 import { provideIcons } from '@ng-icons/core';
@@ -11,11 +11,21 @@ import { hugeAdd01 } from '@ng-icons/huge-icons';
   imports: [RouterOutlet, RouterLink, ...TRANSLATE_IMPORTS],
   viewProviders: [provideIcons({ hugeAdd01 })],
 })
-export class CatalogPage extends BaseComponent implements OnInit {
-  router = inject(ActivatedRoute);
+export class CatalogPage extends BaseComponent {
+  activatedRouter = inject(ActivatedRoute);
   hugeAdd01 = 'hugeAdd01';
 
-  ngOnInit() {
-    return this.router.children;
+  router = inject(Router);
+
+  getPageTitle(): string {
+    const url = this.router.url;
+
+    if (url === '/catalog' || url === '/catalog/') {
+      return 'admin.catalogPage.mainTitle';
+    } else if (url.includes('/catalog/add-product')) {
+      return 'admin.catalogPage.addProductTitle';
+    } else {
+      return 'admin.catalogPage.mainTitle';
+    }
   }
 }
--- src/app/shared/components/admin-nav/admin-nav.ts
+++ src/app/shared/components/admin-nav/admin-nav.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
-import { provideIcons } from '@ng-icons/core';
-import { hugeDashboardBrowsing, hugeCatalogue, hugeUserMultiple } from '@ng-icons/huge-icons';
+import { Component, inject } from '@angular/core';
+import { provideIcons, NgIcon } from '@ng-icons/core';
+import {
+  hugeDashboardBrowsing,
+  hugeCatalogue,
+  hugeUserMultiple,
+  hugeLogin02,
+  hugeDeliveryBox01,
+} from '@ng-icons/huge-icons';
 import { AdminNavLink } from '../design-system/admin-nav-link/admin-nav-link';
 import { TranslateButtons } from '../design-system/translate-buttons/translate-buttons';
 import { AdminOnlyDirective } from '../../directives/admin-only/admin-only.directive';
+import { AuthServices } from '@Features/auth/services/auth';
 
 @Component({
   selector: 'app-admin-nav',
   templateUrl: './admin-nav.html',
-  imports: [AdminNavLink, TranslateButtons, TranslateButtons, AdminOnlyDirective],
-  viewProviders: [provideIcons({ hugeDashboardBrowsing, hugeCatalogue, hugeUserMultiple })],
+  imports: [AdminNavLink, TranslateButtons, TranslateButtons, AdminOnlyDirective, NgIcon],
+  viewProviders: [
+    provideIcons({
+      hugeDashboardBrowsing,
+      hugeCatalogue,
+      hugeUserMultiple,
+      hugeLogin02,
+      hugeDeliveryBox01,
+    }),
+  ],
 })
 export class AdminNav {
+  authServices = inject(AuthServices);
   hugeDashboardBrowsing = 'hugeDashboardBrowsing';
   hugeCatalogue = 'hugeCatalogue';
   hugeUserMultiple = 'hugeUserMultiple';
+  hugeLogin02 = 'hugeLogin02';
+  hugeDeliveryBox01 = 'hugeDeliveryBox01';
+
+  logout() {
+    this.authServices.logout();
+    window.location.reload();
+  }
 }
