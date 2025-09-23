cdf467fbfd01276ceb09c6e76c7f4c2cedfdafb4
LouisR17 <roccalouis26@gmail.com>
2025-09-23 10:40:22 +0200

    fix: fix config service worker and style

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -37,6 +37,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          darkModeSelector: false,
+        },
       },
     }),
     MessageService,
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,16 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
   standalone: true,
   host: {
     '[attr.data-fix-primeng]': 'true',
   },
 })
-export class App {
-  constructor() {
-    window.addEventListener('offline', () => {
-      window.location.href = '/offline.html';
-    });
-  }
-}
+export class App {}
